e2396240c2f51a3f755cf67370c99b17ffaba2d7
Alejandro Casanas <casanas10@gmail.com>
2017-02-13 22:39:13 -0500

    first commit

---
+++ CUBANR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CUBANR
+//
+//  Created by alejandro casanas on 2/13/17.
+//  Copyright © 2017 alejandro casanas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
